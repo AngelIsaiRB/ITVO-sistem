b26cc385149a2a37821259324b2267c5dbaeaded
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2021-04-06 22:46:37 -0500

    init login ok

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta:{protected :true}
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  
 ]
 
 const router = createRouter({
@@ -22,4 +22,17 @@ const router = createRouter({
   routes
 })
 
+router.beforeEach((to,from,next)=>{
+  if(to.meta.protected){
+    if(localStorage.getItem("loged")){
+      next();
+    }
+    else{
+      next("/login");
+    }
+  }
+  else{
+    next();
+  }
+})
 export default router
