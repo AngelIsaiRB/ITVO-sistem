7cfb316f59c86c75407b10e1a079f2864c89996a
AngelIsai <63125934+Anegelisai@users.noreply.github.com>
2021-04-25 23:53:32 -0500

    form add styling and more atributes

--- src/components/modals/NewProyect.vue
+++ src/components/modals/NewProyect.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="z-30 top-0 absolute h-screen w-screen bg-black bg-opacity-50">
       <div class="flex justify-center items-center w-full h-full">
-      <div class="w-8/12 max-w-xl h-5/6  bg-white rounded-lg">
+      <div class="w-8/12 max-w-xl h-auto py-10 bg-white rounded-lg">
       <div class="w-full flex justify-end cursor-pointer"
             @click="fucntionClose()"    
             >
-          <p class="text-black text-2xl my-2 mx-4">X</p>
+          <p class="text-black text-2xl  mx-4">X</p>
       </div>
         <div class="w-full mt-10">
-            <p class="text-base md:text-2xl text-center">Nuevo proyecto</p>
+            <p class="text-base md:text-2xl font-bold text-center">Nuevo proyecto</p>
         </div>
         <div class="mt-6 mx-4">
             <p class="text-sm md:text-xl">tipo de proyecto:</p>
@@ -25,6 +25,33 @@
                     type="text"
                     placeholder="nombre"
                     >
+        </div>
+        <div class="mx-4 mt-2">
+             <p class="text-sm md:text-xl">Nombre de Proyecto</p>
+             <input 
+                    class="bg-gray-100 border-2 border-black"
+                    type="text"
+                    placeholder="nombre"
+                    >
+        </div>
+        <div class="mt-6 mx-4">
+            <p class="text-sm md:text-xl">Carrera:</p>
+            <select name="carrera" id="carrera">
+                <option value="1">Tics </option>
+                <option value="2">Informatica</option>
+            </select>
+        </div>
+        <div class="mx-4 mt-mt-6 mx-4">
+            <p class="text-sm md:text-xl">Asesor</p>
+            <select name="assesor" id="asesor">
+                <option value="p1">Profesor 1</option>
+                <option value="p2">Profesor</option>
+                <option value="p3">Profesor</option>
+            </select>
+        </div>
+        <div class="w-full flex justify-end">
+            <div><button class="px-6 mx-4 py-2 text-black font-bold text-base bg-red-400 hover:bg-red-600   rounded-xl" >Cancelar</button></div>
+            <div><button class="px-6 mx-4 py-2 text-black font-bold text-base bg-green-300 hover:bg-green-600   rounded-xl" >Crear</button></div>
         </div>
       </div>
       </div>
