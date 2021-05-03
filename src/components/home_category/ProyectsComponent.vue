4919241c9363dbf22aa9b4b176fd1b925638a483
AngelIsai <63125934+Anegelisai@users.noreply.github.com>
2021-05-02 22:39:52 -0500

    responsive changes

--- src/components/home_category/ProyectsComponent.vue
+++ src/components/home_category/ProyectsComponent.vue
@@ -20,7 +20,7 @@
             </svg>
           </div>
         </div>
-        <div class="grid grid-cols-4 gap-4" >
+        <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6" >
           <div class="flex mt-5 cursor-pointer "  @click="openModalNew()">
             <div class="max-w-xs rounded overflow-hidden shadow-lg my-2py-4 border border-gray-300 hover:bg-gray-100">
               <img class="h-28 px-8" src="https://ssl.gstatic.com/analytics/rap/20210322_00020000/static/pngs/blank_google_add_2x.png" alt="Sunset in the mountains"/>
