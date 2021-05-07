4936335e86eb61ee8ffccd4afbc4026ba58c7c4e
AngelIsai <63125934+Anegelisai@users.noreply.github.com>
2021-05-07 00:21:58 -0500

    Alert component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
+<AlertPopUpComponentVue />
   <router-view/>
 </template>
+
+<script>
+import AlertPopUpComponentVue from './components/popUp/AlertPopUpComponent.vue'
+export default {
+  components: {
+    AlertPopUpComponentVue,
+  },
+}
+</script>
 
 <style>
 
