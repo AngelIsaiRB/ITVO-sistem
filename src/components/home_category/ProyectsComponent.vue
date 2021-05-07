0be110527884872713b97da1bac2792da926bcbb
AngelIsai <63125934+Anegelisai@users.noreply.github.com>
2021-05-06 23:10:01 -0500

    componente card

--- src/components/home_category/ProyectsComponent.vue
+++ src/components/home_category/ProyectsComponent.vue
@@ -30,56 +30,18 @@
               </div>
             </div>
           </div>
-          <div class="flex mt-5 cursor-pointer ">
-            <div class="max-w-xs rounded overflow-hidden shadow-lg my-2py-4 border border-gray-300 hover:bg-gray-100">
-              <img class="h-28 px-8" src="http://viplifezorrilla.com/sisMercadeo/public/img/usuario.jpg" alt="Sunset in the mountains"/>
-              <div class="px-2 py-2">
-                <div class="font-semibold text-sm mb-2">Nombre de alumno</div>
-                <p class="font-semibold text-gray-400 text-xs">Residencia</p>
-              </div>
-            </div>
-          </div>
-          <div class="flex mt-5 cursor-pointer ">
-            <div class="max-w-xs rounded overflow-hidden shadow-lg my-2py-4 border border-gray-300 hover:bg-gray-100">
-              <img class="h-28 px-8" src="http://viplifezorrilla.com/sisMercadeo/public/img/usuario.jpg" alt="Sunset in the mountains"/>
-              <div class="px-2 py-2">
-                <div class="font-semibold text-sm mb-2">Nombre de alumno</div>
-                <p class="font-semibold text-gray-400 text-xs">Residencia</p>
-              </div>
-            </div>
-          </div>
-          <div class="flex mt-5 cursor-pointer ">
-            <div class="max-w-xs rounded overflow-hidden shadow-lg my-2py-4 border border-gray-300 hover:bg-gray-100">
-              <img class="h-28 px-8" src="http://viplifezorrilla.com/sisMercadeo/public/img/usuario.jpg" alt="Sunset in the mountains"/>
-              <div class="px-2 py-2">
-                <div class="font-semibold text-sm mb-2">Nombre de alumno</div>
-                <p class="font-semibold text-gray-400 text-xs">Residencia</p>
-              </div>
-            </div>
-          </div>
-          <div class="flex mt-5 cursor-pointer ">
-            <div class="max-w-xs rounded overflow-hidden shadow-lg my-2py-4 border border-gray-300 hover:bg-gray-100">
-              <img class="h-28 px-8" src="http://viplifezorrilla.com/sisMercadeo/public/img/usuario.jpg" alt="Sunset in the mountains"/>
-              <div class="px-2 py-2">
-                <div class="font-semibold text-sm mb-2">Nombre de alumno</div>
-                <p class="font-semibold text-gray-400 text-xs">Residencia</p>
-              </div>
-            </div>
-          </div>
-          <div class="flex mt-5 cursor-pointer ">
-            <div class="max-w-xs rounded overflow-hidden shadow-lg my-2py-4 border border-gray-300 hover:bg-gray-100">
-              <img class="h-28 px-8" src="http://viplifezorrilla.com/sisMercadeo/public/img/usuario.jpg" alt="Sunset in the mountains"/>
-              <div class="px-2 py-2">
-                <div class="font-semibold text-sm mb-2">Nombre de alumno</div>
-                <p class="font-semibold text-gray-400 text-xs">Residencia</p>
-              </div>
-            </div>
+          <div v-for="x in (10)" :key="x" >
+            <CardAlimnoProyectsVue />
           </div>
         </div>
 </template>
 
 <script>
+import CardAlimnoProyectsVue from '../cards/CardAlimnoProyects.vue'
 export default {
+  components: {
+    CardAlimnoProyectsVue,
+  },
     props: {
         openModalNew: {
             type: Function,
