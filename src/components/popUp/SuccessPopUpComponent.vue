7342bf9c12ebf35ce746d9083db98b378f3feba2
AngelIsai <63125934+Anegelisai@users.noreply.github.com>
2021-05-09 22:49:42 -0500

    succes and warning alert

---
+++ src/components/popUp/SuccessPopUpComponent.vue
@@ -0,0 +1,36 @@
+<template>
+<div v-if="active">
+  <div class="z-50 fixed top-0">
+    <div @click="onCloseAlert()" class="w-screen flex justify-center min-w-min mt-4 h-12 cursor-pointer">
+      <div class="bg-green-200 border flex border-green-400">
+        <div class="block flex items-center rounded-sm" role="alert">
+          <div class="text-sm text-left text-green-600 mx-10">
+            ¡Todo ok!
+          </div>
+        </div>
+        <div class="justify-end">
+          <i class="fas fa-times mx-1"></i>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+    
+   data() {
+       return {
+           active: true
+       }
+   },
+   methods: {
+       onCloseAlert() {
+           this.active =false
+       }
+   },
+};
+</script>
+
+<style></style>
---
+++ src/components/popUp/WarningPopUpComponent.vue
@@ -0,0 +1,36 @@
+<template>
+<div v-if="active">
+  <div class="z-50 fixed top-0">
+    <div @click="onCloseAlert()" class="w-screen flex justify-center min-w-min mt-4 h-12 cursor-pointer">
+      <div class="bg-yellow-200 border flex border-yellow-400">
+        <div class="block flex items-center rounded-sm" role="alert">
+          <div class="text-sm text-left text-yellow-600 mx-10">
+            ¡alerta media!
+          </div>
+        </div>
+        <div class="justify-end">
+          <i class="fas fa-times mx-1"></i>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+    
+   data() {
+       return {
+           active: true
+       }
+   },
+   methods: {
+       onCloseAlert() {
+           this.active =false
+       }
+   },
+};
+</script>
+
+<style></style>
