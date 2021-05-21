6d8782292d4b47663203272df1ab024d7e47eca6
AngelIsai <63125934+Anegelisai@users.noreply.github.com>
2021-05-20 21:22:45 -0500

    antes de awesome icons

---
+++ src/components/menus/AdminMenuComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="w-full h-full">
+    <div class="grid grid-cols-4 w-full h-full">
+        <div class=" w-full h-full flex justify-center items-center">
+            <button class="bg-green-300 h-4/6 w-10/12 hover:bg-blue-500">
+               <p class="text-black font-black"> Agregar profesor</p>
+            </button>
+        </div>        
+        <div class="bg-blue-400 w-full h-full "></div>    
+        <div class="bg-purple-400 w-full h-full "></div>    
+        <div class="bg-green-400 w-full h-full "></div>    
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/admin/AdminComponent.vue
+++ src/views/admin/AdminComponent.vue
@@ -5,8 +5,13 @@
           <div>
             <MenuAdminAsideComponentVue/>
           </div>
-          <div class="h-full w-full bg-gray-100">
-              <UsersAdminComponentVue/>
+          <div class="h-full w-full bg-gray-100 ">
+              <div>
+                  <div class="h-20 w-full bg-gray-100 my-2 mx-4">
+                      <AdminMenuComponentVue/>
+                  </div>
+                <UsersAdminComponentVue/>
+              </div>
         </div>          
 
       </div>      
@@ -16,12 +21,14 @@
 <script>
 import MenuAdminAsideComponentVue from '../../components/aside/MenuAdminAsideComponent.vue'
 import HeaderComponentVue from '../../components/header/HeaderComponent.vue'
+import AdminMenuComponentVue from '../../components/menus/AdminMenuComponent.vue'
 import UsersAdminComponentVue from '../../components/tables/UsersAdminComponent.vue'
 export default {
     components:{
         HeaderComponentVue,
         MenuAdminAsideComponentVue,
         UsersAdminComponentVue,
+        AdminMenuComponentVue,
     }
 
 }
